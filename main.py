5002499c5086fda44521540463c829136b7ce9a0
davidr3489@gmail.com <crowden3489>
2022-10-25 14:11:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
+
+@app.route('/meet_team')
+def meet_team():
+    return render_template('meet-team.html')
+
+@app.route('/get_involved')
+def get_involved():
+    return render_template('get-involved.html')
+
+@app.route('/further_work')
+def further_work():
+    return render_template('further-work.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
